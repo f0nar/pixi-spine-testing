6c125ae7afa2d7bd5b793fbe65662e2291ce025e
Vladyslav Pohorielov <vpohorielov@playtika.com>
2023-10-17 17:31:12 +0300

    use dynamic import

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,29 @@
 import { Application, Assets } from "pixi.js";
-import { load } from './newSpine';
-// import { load } from './oldSpine';
 
+const useOldSpine = true;
 const spineVersion: 38 | 40 = 40;
 const packingType: 'polygons' | 'rectangles' = 'rectangles';
 const assetsFolder = `${spineVersion}_json_${packingType}`;
 
-const app = new Application<HTMLCanvasElement>({
-  background: "#1099bb",
-  resizeTo: window
-});
+const run = async () => {
+  const { load: loadSpine } = await (
+    useOldSpine ?
+      import(`./oldSpine`) :
+      import(`./newSpine`)
+  ); 
 
-document.body.appendChild(app.view);
+  const app = new Application<HTMLCanvasElement>({
+    background: "#1099bb",
+    resizeTo: window
+  });
+  
+  document.body.appendChild(app.view);
 
-Assets.add('spineData', `${assetsFolder}/skeleton.json`);
-Assets.add('spineAtlas', `${assetsFolder}/skeleton.atlas`);
-Assets.load(['spineData', 'spineAtlas']).then((_) => load(app));
+  Assets.add('spineData', `${assetsFolder}/skeleton.json`);
+  Assets.add('spineAtlas', `${assetsFolder}/skeleton.atlas`);
+  await Assets.load(['spineData', 'spineAtlas']).then(console.log);
 
+  loadSpine(app);
+};
+
+run();

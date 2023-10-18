d9c84469a5332ba01e206f750f6597d7334d7d2b
Vladyslav Pohorielov <vpohorielov@playtika.com>
2023-10-18 12:23:56 +0300

    update spine resources, update graphic properties

--- src/newSpine.ts
+++ src/newSpine.ts
@@ -7,7 +7,6 @@ export const load = (app: Application) => {
 
     const bounds = spine.getBounds(true);
   
-    spine.scale.set(0.5);
     spine.position.set((app.view.width - bounds.width) / 2, (app.view.height - bounds.height) / 2);
     spine.autoUpdate = true;
     spine.state.setAnimation(0, "animation", true);
--- src/oldSpine.ts
+++ src/oldSpine.ts
@@ -9,8 +9,7 @@ export const load = (app: Application) => {
 
   const bounds = spine.getBounds(true);
   
-  spine.scale.set(0.5);
-  spine.position.set((app.view.width - bounds.width) / 2, (app.view.height - bounds.height) / 2);
+  spine.position.set((app.view.width - bounds.width) / 2 - bounds.left, (app.view.height - bounds.height) / 2 - bounds.top);
   spine.state.setAnimation(0, "animation", true);
   spine.autoUpdate = true;
 };
